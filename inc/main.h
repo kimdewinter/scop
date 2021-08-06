6ca8e7e5d2c67e3d0435d18b967f77adde38ccfc
kimthulhu <kdewinter87@protonmail.com>
2021-08-06 17:11:50 +0200

    feat: make able to spawn window

--- inc/main.h
+++ inc/main.h
@@ -6,13 +6,7 @@
 /*   By: kde-wint <kde-wint@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/04 16:28:23 by kde-wint      #+#    #+#                 */
-/*   Updated: 2021/08/04 19:19:24 by kde-wint      ########   odam.nl         */
+/*   Updated: 2021/08/06 16:37:53 by kde-wint      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/includes/libft.h"
-
-void derp(void)
-{
-	ft_printf("Hello world!");
-}
--- src/main.c
+++ src/main.c
@@ -6,15 +6,45 @@
 /*   By: kde-wint <kde-wint@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/04 16:28:28 by kde-wint      #+#    #+#                 */
-/*   Updated: 2021/08/04 19:44:31 by kde-wint      ########   odam.nl         */
+/*   Updated: 2021/08/06 16:55:06 by kde-wint      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
+#include "libft.h"
 #include <stdlib.h>
+#include <SDL.h>
 
 int main(void)
 {
-	derp();
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_GL_SetAttribute(
+		SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+	SDL_Window* window = SDL_CreateWindow(
+		"OpenGL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,
+		SDL_WINDOW_OPENGL);
+
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	SDL_Event windowEvent;
+	while (1)
+	{
+		if (SDL_PollEvent(&windowEvent))
+		{
+			if (windowEvent.type == SDL_QUIT)
+				break;
+		}
+
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_GL_DeleteContext(context);
+
+    SDL_Quit();
 	return EXIT_SUCCESS;
 }
