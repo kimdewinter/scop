5864ed86a55b77652840f3560002a58078d6bcd7
kimthulhu <kdewinter87@protonmail.com>
2021-10-20 21:12:50 +0200

    feat: clean up unnecessaries

--- src/matrix_handler.c
+++ src/matrix_handler.c
@@ -50,24 +50,6 @@ void scale_matrix(float matrix[16], const float scale_multipliers[3])
 	matrix[10] = scale_multipliers[2];
 }
 
-void rotate(t_app *app)
-{
-	float mat4[16] = {
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f
-	};
-	// float test_rotate_vec[3] = { 0.0f, 0.0f, 1.0f };
-	float test_scale_vec[3] = { 0.5f, 0.5f, 0.5f };
-
-	rotate_matrix(mat4, 90.0f, AXIS_Z);
-	scale_matrix(mat4, test_scale_vec);
-	unsigned int transform_location =
-		glGetUniformLocation(app->shader_program, "transform");
-	glUniformMatrix4fv(transform_location, 1, GL_FALSE, mat4);
-}
-
 void transform(t_app *app)
 {
 	float mat4[16] = {
@@ -77,11 +59,9 @@ void transform(t_app *app)
 		0.0f, 0.0f, 0.0f, 1.0f
 	};
 	float test_translate_vec[3] = { 0.5f, -0.5f, 0.0f };
-	// float test_rotate_vec[3] = { 0.0f, 0.0f, 1.0f };
-	// float test_scale_vec[3] = { 0.5f, 0.5f, 0.5f };
 
 	translate_matrix(mat4, test_translate_vec);
-	rotate_matrix(mat4, SDL_GetTicks() / 1000, AXIS_Z);
+	rotate_matrix(mat4, SDL_GetTicks() / 100, AXIS_Z);
 
 	glUseProgram(app->shader_program);
 	unsigned int transform_location = glGetUniformLocation(
