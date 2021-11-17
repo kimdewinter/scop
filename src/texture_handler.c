3abd1a5766db5bd66b7a774ebce6ede0634bb933
kimthulhu <kdewinter87@protonmail.com>
2021-11-17 16:58:22 +0100

    feat: add bmp_loader, not fully displaying correctly yet

--- src/texture_handler.c
+++ src/texture_handler.c
@@ -3,6 +3,8 @@
 
 bool parse_texture_arg(t_app *app, int argc, char *argv[])
 {
+	size_t len;
+
 	if (argc < 2)
 	{
 		fprintf(
@@ -23,13 +25,86 @@ bool parse_texture_arg(t_app *app, int argc, char *argv[])
 		);
 		return (false);
 	}
+	len = strlen(argv[2]);
+	if (len < 5 || strncmp(".bmp", &argv[2][len - 4], 4) != 0)
+	{
+		fprintf(
+			stdout,
+			"Error: %s must receive filename with .bmp filename extension\n",
+			PROGRAM_NAME
+		);
+		return (false);
+	}
 	app->texture_file_name = strdup(argv[2]);
+	return (true);
+}
+
+static bool import_image(
+	const char *file_name,
+	unsigned char **dst_data,
+	unsigned int *dst_width,
+	unsigned int *dst_height
+)
+{
+	// Data read from the header of the BMP file
+	unsigned char header[54];// Each BMP file begins by a 54-bytes header
+	unsigned int dataPos;// Position in the file where the actual data begins
+	unsigned int width, height;
+	unsigned char *data;
+	unsigned int imageSize;// = width*height*3
+	FILE *file = fopen(file_name, "r");
+
+	if (!file)
+	{
+		fprintf(stdout, "Error: failed to open texture file\n");
+		return (false);
+	}
+	if (fread(header, sizeof(unsigned char), 54, file) != 54 ||
+		header[0] != 'B' ||
+		header[1] != 'M')
+	{
+		fclose(file);
+		fprintf(stdout, "Error: texture must be a valid .bmp file\n");
+		return (false);
+	}
+
+	// Read ints from the byte array
+	dataPos = *(int *)&(header[0x0A]);
+	imageSize = *(int *)&(header[0x22]);
+	width = *(int *)&(header[0x12]);
+	height = *(int *)&(header[0x16]);
+
+	// Some BMP files are misformatted, guess missing information
+	if (imageSize == 0)
+		imageSize = width * height * 3; // 3 : one byte for each Red, Green and Blue component
+	if (dataPos == 0)
+		dataPos = 54; // The BMP header is done that way
+
+	// Create a buffer
+	data = (unsigned char *)malloc(imageSize * sizeof(unsigned char));
+	if (!data)
+	{
+		fclose(file);
+		fprintf(stdout, "Error: misallocation in import_image\n");
+		return (false);
+	}
+
+	// Read the actual data from the file into the buffer
+	fread(data, sizeof(unsigned char), imageSize, file);
+
+	//Everything is in memory now, the file can be closed
+	fclose(file);
+
+	*dst_data = data;
+	*dst_width = width;
+	*dst_height = height;
 	return (true);
 }
 
 bool load_texture(t_app *app)
 {
-	int width, height, nrChannels;
+	unsigned char *data;
+	unsigned int width, height;
 
 	// load and create a texture 
 	// -------------------------
@@ -42,15 +117,13 @@ bool load_texture(t_app *app)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	// load image, create texture and generate mipmaps
-	stbi_set_flip_vertically_on_load(true);
-	unsigned char *data = stbi_load(
+	if (import_image(
 		app->texture_file_name,
+		&data,
 		&width,
-		&height,
-		&nrChannels,
-		0
-	);
-	if (data)
+		&height
+		) &&
+		data)
 	{
 		glTexImage2D(
 			GL_TEXTURE_2D,
@@ -59,7 +132,7 @@ bool load_texture(t_app *app)
 			width,
 			height,
 			0,
-			GL_RGB,
+			GL_BGR,
 			GL_UNSIGNED_BYTE,
 			data
 		);
@@ -68,24 +141,11 @@ bool load_texture(t_app *app)
 	else
 	{
 		printf("Failed to load texture\n");
+		return (false);
 	}
-	stbi_image_free(data);
 
+	free(data);
 	glUseProgram(app->shader_program);
 	glUniform1i(glGetUniformLocation(app->shader_program, "texture1"), 0);
-
-	return true;
+	return (true);
 }
-
-// bool load_texture_new(t_app *app, char const * const file_name)
-// {
-// 	GLuint texture;
-// 	int width, height;
-// 	unsigned char *data;
-// 	FILE *file;
-
-// 	file = fopen(file_name, "rb");
-// 	if (!file)
-// 		return false;
-// 	width 
-// }
