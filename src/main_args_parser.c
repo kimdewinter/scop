bf18190f7e9d186b7476f7a2938c470a52315e46
kimthulhu <kdewinter87@protonmail.com>
2021-11-19 16:55:13 +0100

    feat: refactor main_args_parser.c

--- src/main_args_parser.c
+++ src/main_args_parser.c
@@ -1,22 +1,29 @@
 #include "main.h"
 #include <assert.h>
 
+//argument file_extension also requires the period
+static bool has_file_extension(
+    char const * const file_extension,
+    char const * const file_name)
+{
+    size_t len = strlen(file_name);
+
+    //ensure file name is long enough (shortest possible example: a.xyz)
+    if (!(len > 4))
+        return (false);
+    //ensure file name ends in the file extension
+    if (!(strncmp(file_extension, &file_name[len - 4], 4) == 0))
+        return (false);
+    return (true);
+}
+
 bool parse_main_args(t_app *app, const int argc, char const ** const argv)
 {
-    //program must receive .obj object file (.bmp texture file optional)
+    //program must receive .obj object file, and optionally a .bmp texture file
     assert(argc == 2 || argc == 3);
-    //ensure .obj filename is long enough (example: a.obj)
-    assert(strlen(argv[1]) > 4);
-    //ensure .obj filename ends with .obj extension
-    assert(strncmp(".obj", &argv[1][strlen(argv[1]) - 4], 4) == 0);
-    //program might receive .bmp texture file
+    assert(has_file_extension(".obj", argv[1]));
     if (argc == 3)
-    {
-        //ensure .bmp filename is long enough (example: a.bmp)
-        assert(strlen(argv[2]) > 4);
-        //ensure .bmp filename ends with .bmp extension
-        assert(strncmp(".bmp", &argv[2][strlen(argv[2]) - 4], 4) == 0);
-    }
+        assert(has_file_extension(".bmp", argv[2]));
 
     //parsing complete, now store main args
     app->obj_file_name = strdup(argv[1]);
