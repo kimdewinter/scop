382456637718cb5297ceb9038c278621bf04c035
kimthulhu <kdewinter87@protonmail.com>
2021-11-22 15:24:38 +0100

    feat: refactor main_args_parser.c

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ static int shutdown(t_app *app, int exit_return)
 	return (exit_return);
 }
 
-int main(int argc, char const ** const argv)
+int main(int argc, char const **const argv)
 {
 	t_app app;
 
@@ -28,10 +28,9 @@ int main(int argc, char const ** const argv)
 	if (!init_opengl())
 		return (shutdown(&app, EXIT_FAILURE));
 	if (!compile_shader_program(
-		&app.shader_program,
-		"vertex_shader.vert",
-		"fragment_shader.frag"
-	))
+			&app.shader_program,
+			"vertex_shader.vert",
+			"fragment_shader.frag"))
 		return (shutdown(&app, EXIT_FAILURE));
 	if (!load_obj(&app, app.obj_file_name))
 		return (shutdown(&app, EXIT_FAILURE));
@@ -43,15 +42,13 @@ int main(int argc, char const ** const argv)
 			return (shutdown(&app, EXIT_FAILURE));
 		glUniform1i(
 			glGetUniformLocation(app.shader_program, "texture_provided"),
-			1
-		);
+			1);
 	}
 	else
 	{
 		glUniform1i(
 			glGetUniformLocation(app.shader_program, "texture_provided"),
-			0
-		);
+			0);
 	}
 	glViewport(0, 0, DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT);
 	app.close_window = false;
@@ -61,41 +58,39 @@ int main(int argc, char const ** const argv)
 			(shutdown(&app, EXIT_FAILURE));
 		handle_transformations(&app);
 		// render
-        // ------
-        glClearColor(
+		// ------
+		glClearColor(
 			DEFAULT_CLEARCOLOR_RED,
 			DEFAULT_CLEARCOLOR_GREEN,
 			DEFAULT_CLEARCOLOR_BLUE,
-			DEFAULT_CLEARCOLOR_ALPHA
-		);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			DEFAULT_CLEARCOLOR_ALPHA);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, app.texture);
 
-        // draw our first triangle
-        glUseProgram(app.shader_program);
-        glBindVertexArray(app.VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+		// draw our first triangle
+		glUseProgram(app.shader_program);
+		glBindVertexArray(app.VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 		if (app.indices_length > 0)
 			glDrawElements(
 				GL_TRIANGLES,
 				app.indices_length,
 				GL_UNSIGNED_INT,
-				0
-			);
+				0);
 		else
 			glDrawArrays(GL_TRIANGLES, 0, app.vertices_length);
-        // glBindVertexArray(0); // no need to unbind it every time
+		// glBindVertexArray(0); // no need to unbind it every time
 		SDL_GL_SwapWindow(app.sdl.window);
 	}
 
 	// optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
-    glDeleteVertexArrays(1, &app.VAO);
-    glDeleteBuffers(1, &app.VBO);
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &app.VAO);
+	glDeleteBuffers(1, &app.VBO);
 	if (app.indices_length > 0)
-    	glDeleteBuffers(1, &app.EBO);
-    glDeleteProgram(app.shader_program);
+		glDeleteBuffers(1, &app.EBO);
+	glDeleteProgram(app.shader_program);
 
-    return (shutdown(&app, EXIT_SUCCESS));
+	return (shutdown(&app, EXIT_SUCCESS));
 }
--- src/main_args_parser.c
+++ src/main_args_parser.c
@@ -1,23 +1,27 @@
 #include "main.h"
 #include <assert.h>
 
-//argument file_extension also requires the period
+//argument "file_extension" must include the period
 static bool has_file_extension(
-    char const * const file_extension,
-    char const * const file_name)
+    char const *const file_extension,
+    char const *const file_name)
 {
-    size_t len = strlen(file_name);
+    size_t file_len = strlen(file_name);
+    size_t ext_len = strlen(file_extension);
 
-    //ensure file name is long enough (shortest possible example: a.xyz)
-    if (!(len > 4))
+    if (ext_len < 4)
         return (false);
-    //ensure file name ends in the file extension
-    if (!(strncmp(file_extension, &file_name[len - 4], 4) == 0))
+    if (file_len < (ext_len + 1))
+        return (false);
+    if (strncmp(
+            file_extension,
+            &file_name[file_len - ext_len],
+            ext_len) != 0)
         return (false);
     return (true);
 }
 
-bool parse_main_args(t_app *app, const int argc, char const ** const argv)
+bool parse_main_args(t_app *app, const int argc, char const **const argv)
 {
     //program must receive .obj object file, and optionally a .bmp texture file
     assert(argc == 2 || argc == 3);
@@ -31,7 +35,7 @@ bool parse_main_args(t_app *app, const int argc, char const ** const argv)
     if (argc == 3)
     {
         app->texture_file_name = strdup(argv[2]);
-        assert(app->obj_file_name);
+        assert(app->texture_file_name);
     }
     return (true);
 }
