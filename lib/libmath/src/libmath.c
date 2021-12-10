8845be60927b3c4f013d6f49f8130e9857f1fac6
kimthulhu <kdewinter87@protonmail.com>
2021-12-10 18:45:02 +0100

    fix: rotation broken

--- lib/libmath/src/libmath.c
+++ lib/libmath/src/libmath.c
@@ -7,26 +7,35 @@
 void get_identity_mat4(t_mat4 *dst)
 {
     assert(dst);
-    memcpy(
-        dst,
-        (t_mat4){1.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 1.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 1.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 1.0f},
-        sizeof(t_mat4));
+    memset(dst, 0, sizeof(t_mat4));
+    (*dst)[0] += 1.0f;
+    (*dst)[5] += 1.0f;
+    (*dst)[10] += 1.0f;
+    (*dst)[15] += 1.0f;
+    // memcpy(
+    //     dst,
+    //     (t_mat4){1.0f, 0.0f, 0.0f, 0.0f,
+    //              0.0f, 1.0f, 0.0f, 0.0f,
+    //              0.0f, 0.0f, 1.0f, 0.0f,
+    //              0.0f, 0.0f, 0.0f, 1.0f},
+    //     sizeof(t_mat4));
 }
 
 //The caller must allocate the space for arg "dst"
 void get_scaling_mat4(t_mat4 *dst, float const x, float const y, float const z)
 {
     assert(dst);
-    memcpy(
-        dst,
-        (t_mat4){1.0f + x, 0.0f, 0.0f, 0.0f,
-                 0.0f, 1.0f + y, 0.0f, 0.0f,
-                 0.0f, 0.0f, 1.0f + z, 0.0f,
-                 0.0f, 0.0f, 0.0f, 1.0f},
-        sizeof(t_mat4));
+    get_identity_mat4(dst);
+    (*dst)[0] += x;
+    (*dst)[5] += y;
+    (*dst)[10] += z;
+    // memcpy(
+    //     dst,
+    //     (t_mat4){1.0f + x, 0.0f, 0.0f, 0.0f,
+    //              0.0f, 1.0f + y, 0.0f, 0.0f,
+    //              0.0f, 0.0f, 1.0f + z, 0.0f,
+    //              0.0f, 0.0f, 0.0f, 1.0f},
+    //     sizeof(t_mat4));
 }
 
 //The caller must allocate the space for arg "dst"
@@ -42,30 +51,49 @@ void get_rotation_mat4(
     t_mat4 intermediate_mat;
 
     assert(dst);
+
     //Create x rotation matrix
-    memcpy(
-        &x_mat,
-        (t_mat4){1.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, (float)cos((double)x_deg), (float)sin((double)x_deg), 0.0f,
-                 0.0f, (float)(-(sin((double)x_deg))), (float)cos((double)x_deg), 0.0f,
-                 0.0f, 0.0f, 0.0f, 1.0f},
-        sizeof(t_mat4));
+    get_identity_mat4(&x_mat);
+    x_mat[5] = cos((double)x_deg);
+    x_mat[6] = sin((double)x_deg);
+    x_mat[9] = -sin((double)x_deg);
+    x_mat[10] = cos((double)x_deg);
+    // memcpy(
+    //     &x_mat,
+    //     (t_mat4){1.0f, 0.0f, 0.0f, 0.0f,
+    //              0.0f, (float)cos((double)x_deg), (float)sin((double)x_deg), 0.0f,
+    //              0.0f, (float)(-(sin((double)x_deg))), (float)cos((double)x_deg), 0.0f,
+    //              0.0f, 0.0f, 0.0f, 1.0f},
+    //     sizeof(t_mat4));
+
     //Create y rotation matrix
-    memcpy(
-        &y_mat,
-        (t_mat4){(float)cos((double)y_deg), 0.0f, (float)(-(sin((double)y_deg))), 0.0f,
-                 0.0f, 1.0f, 0.0f, 0.0f,
-                 (float)sin((double)y_deg), 0.0f, (float)cos((double)y_deg), 0.0f,
-                 0.0f, 0.0f, 0.0f, 1.0f},
-        sizeof(t_mat4));
+    get_identity_mat4(&y_mat);
+    y_mat[0] = cos((double)y_deg);
+    y_mat[2] = -sin((double)y_deg);
+    y_mat[8] = sin((double)y_deg);
+    y_mat[10] = cos((double)y_deg);
+    // memcpy(
+    //     &y_mat,
+    //     (t_mat4){(float)cos((double)y_deg), 0.0f, (float)(-(sin((double)y_deg))), 0.0f,
+    //              0.0f, 1.0f, 0.0f, 0.0f,
+    //              (float)sin((double)y_deg), 0.0f, (float)cos((double)y_deg), 0.0f,
+    //              0.0f, 0.0f, 0.0f, 1.0f},
+    //     sizeof(t_mat4));
+
     //Create z rotation matrix
-    memcpy(
-        &z_mat,
-        (t_mat4){(float)cos((double)z_deg), (float)sin((double)z_deg), 0.0f, 0.0f,
-                 (float)(-(sin((double)z_deg))), (float)cos((double)z_deg), 0.0f, 0.0f,
-                 0.0f, 0.0f, 1.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 1.0f},
-        sizeof(t_mat4));
+    get_identity_mat4(&z_mat);
+    z_mat[0] = cos((double)z_deg);
+    z_mat[1] = sin((double)z_deg);
+    z_mat[4] = -sin((double)z_deg);
+    z_mat[5] = cos((double)z_deg);
+    // memcpy(
+    //     &z_mat,
+    //     (t_mat4){(float)cos((double)z_deg), (float)sin((double)z_deg), 0.0f, 0.0f,
+    //              (float)(-(sin((double)z_deg))), (float)cos((double)z_deg), 0.0f, 0.0f,
+    //              0.0f, 0.0f, 1.0f, 0.0f,
+    //              0.0f, 0.0f, 0.0f, 1.0f},
+    //     sizeof(t_mat4));
+
     //Combine the matrices and output to "dst"
     multiply_mat4(&intermediate_mat, &x_mat, &y_mat);
     multiply_mat4(dst, &intermediate_mat, &z_mat);
@@ -79,13 +107,17 @@ void get_translation_mat4(
     float const z)
 {
     assert(dst);
-    memcpy(
-        dst,
-        (t_mat4){1.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 1.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 1.0f, 0.0f,
-                 0.0f + x, 0.0f + y, 0.0f + z, 1.0f},
-        sizeof(t_mat4));
+    get_identity_mat4(dst);
+    (*dst)[12] += x;
+    (*dst)[13] += y;
+    (*dst)[14] += z;
+    // memcpy(
+    //     dst,
+    //     (t_mat4){1.0f, 0.0f, 0.0f, 0.0f,
+    //              0.0f, 1.0f, 0.0f, 0.0f,
+    //              0.0f, 0.0f, 1.0f, 0.0f,
+    //              0.0f + x, 0.0f + y, 0.0f + z, 1.0f},
+    //     sizeof(t_mat4));
 }
 
 //The caller must allocate the space for arg "dst"
@@ -98,7 +130,7 @@ void multiply_mat4(
     t_mat4 output;
 
     assert(dst && src1 && src2);
-    get_identity_mat4(&output);
+    memset(&output, 0, sizeof(t_mat4));
     for (unsigned int y = 0; y < 4; y++)
         for (unsigned int x = 0; x < 4; x++)
             for (unsigned int i = 0; i < 4; i++)
