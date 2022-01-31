5853eaf38ae1227df5e18a057800ba1c36bd0bc4
kimthulhu <kdewinter87@protonmail.com>
2022-01-31 11:06:46 +0100

    feat: add error msg when unable to open obj file

--- src/obj_loader.c
+++ src/obj_loader.c
@@ -7,7 +7,10 @@ bool load_obj(t_app *app, char const *const file_name)
 	t_obj *obj = obj_import(file_name);
 
 	if (!obj)
+	{
+		fprintf(stdout, "Error: failed to open object file\n");
 		return (false);
+	}
 	app->vertices_length = obj->vertices_len;
 	app->vertices = obj->vertices;
 	app->indices_length = obj->indices_len;
