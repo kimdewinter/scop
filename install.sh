8c218d0f2d30bb0e3c6757b972dc7a614071092b
kimthulhu <kdewinter87@protonmail.com>
2022-02-11 20:48:28 +0100

    fix: wrong paths in install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 #brew install SDL2
 
-export C_INCLUDE_PATH=~/.brew/include
-export LIBRARY_PATH=~/.brew/lib
+export C_INCLUDE_PATH=~/homebrew/Cellar/sdl2/2.0.5/include/
+export LIBRARY_PATH=~/homebrew/Cellar/sdl2/2.0.5/lib
