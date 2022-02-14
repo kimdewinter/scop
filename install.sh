f9ffa0f3ef9ff47a1eb9508dbe8ab507580c00a5
kimthulhu <kdewinter87@protonmail.com>
2022-02-14 11:35:01 +0100

    feat: re-add own cluster pc SDL location

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@
 
 export C_INCLUDE_PATH=~/homebrew/Cellar/sdl2/2.0.5/include/
 export LIBRARY_PATH=~/homebrew/Cellar/sdl2/2.0.5/lib
+# Alternatively:
+#export C_INCLUDE_PATH=~/.brew/include
+#export LIBRARY_PATH=~/.brew/lib
